31e9d3c0fc6dfec1822c23a05d520863157fc0a9
LucasAL11 <lucasluna1000@gmail.com>
2024-09-15 23:23:39 -0300

    Setup projetct

---
+++ mov-estoque/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace mov_estoque.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<string>> AddProducts()
+        {
+            try
+            {
+                return Ok("teste");
+            }
+            catch (Exception ex) 
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
